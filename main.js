23f9771c48a06fd486671b308aeebc6f93d442fe
Kashima <project-red-alice@outlook.com>
2021-10-26 20:51:15 +1100

    fixed internal error

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const express = require("express"),
     bodyParser = require("body-parser");
 
 const app = express();
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(require("express-session")({
