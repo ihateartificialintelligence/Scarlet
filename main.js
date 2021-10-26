122bb062580135d9975ae170f1e5440441e54f53
Kashima <project-red-alice@outlook.com>
2021-10-26 20:46:09 +1100

    Changed the API method

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server = require("./src/client");
-server.App.fullStart((process.env.PORT), "");
+const express = require("express"),
+    bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require("express-session")({
+    secret: `${Math.floor(Math.random()*5000)}`,
+    resave: false,
+    saveUninitialized: false
+}));
+
+const http = require('http');
+const server = http.createServer(app);
+
+//=====================
+// ROUTES
+//=====================
+
+// Showing home page
+app.get("/", function (req, res) {
+    res.render("login");
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log("Server Has Started!");
+});
