d1e01337801c7b7d7010ad2db79d6817efe9cb4e
Kashima <project-red-alice@outlook.com>
2021-11-16 10:38:02 +1100

    Added webhooks for monitoring

--- index.js
+++ index.js
@@ -35,10 +35,37 @@ app.use(function (req, res, next) {
 app.get('/docs', (req, res) => res.redirect("https://docs.scarletai.xyz"));
 var request = require('request');
 
+// Send IP logs to IP Investigator to watch
+// for IPs in event of a (Re[D])DoS
 request('https://ipinvestigator.expeditedaddons.com/?api_key=NZF0IYA5QSCERM37B2D560418XWV36H2OK9T41ULJ9PG78&ip=68.10.149.45', function (error, response, body) {
     console.log('Status:', response.statusCode);
     console.log('Headers:', JSON.stringify(response.headers));
     console.log('Response:', body);
+});
+
+// API Webhook handler to log Heroku Updates
+// to Discord channel: 909948054961016864 (scarlets-logs)
+app.post("/webhook", async (req, res) => {
+    const Payload = req.body;
+   //Respond To Heroku Webhook
+    res.sendStatus(200);
+    console.log(Object.keys(Payload));
+    const options = {
+        method: "POST",
+        url:
+            "https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU",
+        headers: {
+            "Content-type": "application/json",
+        },
+        //Format JSON DATA
+        body: JSON.stringify({
+            content: `Hi There!\nA new update has been pushed to my server! \nServer-name: \`${Payload.data.app.name}\`\nUpdate: \`Unkown\`\nVersion: \`${Payload.data.app.version}`
+        }),
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response);
+    });
 });
 
 
