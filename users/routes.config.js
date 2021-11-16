9b0b718e9ff0613d4b74d947023700fbcab60fef
Kashima <project-red-alice@outlook.com>
2021-11-16 11:09:00 +1100

    Fixed error with IP handler

--- users/routes.config.js
+++ users/routes.config.js
@@ -1,4 +1,5 @@
 const UsersController = require('./controllers/users.controller');
+const request = require('request');
 
 exports.routesConfig = function (app) {
     app.post('/users', [
@@ -29,4 +30,26 @@ exports.routesConfig = function (app) {
     app.get('/docs', [
         UsersController.docs
     ]);
+    app.post("webhooks", (req, res) => {
+        const Payload = req.body;
+        //Respond To Heroku Webhook
+        res.sendStatus(200);
+        console.log(Object.keys(Payload));
+        const options = {
+            method: "POST",
+            url:
+                "https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU",
+            headers: {
+                "Content-type": "application/json",
+            },
+            //Format JSON DATA
+            body: JSON.stringify({
+                content: `Hi There!\nA new update has been pushed to my server! \nServer-name: \`${Payload.data.app.name}\`\nUpdate: \`Unkown\`\nVersion: \`${Payload.data.app.version}`
+            }),
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response);
+        });
+    })
 };
