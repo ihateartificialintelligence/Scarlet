2bd707a452697500d8175f11c6ddbc8dae421183
Kashima <project-red-alice@outlook.com>
2021-11-19 09:58:53 +1100

    changed post to use `node-fetch`

--- users/controllers/webhook.controller.js
+++ users/controllers/webhook.controller.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const request = require("request");
 
 exports.toDiscord = (req, res) => {
@@ -5,20 +6,20 @@ exports.toDiscord = (req, res) => {
     //Respond To Heroku Webhook
     res.sendStatus(200);
     console.log(Object.keys(Payload));
-    const options = {
-        method: "POST",
-        url:
-            "https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU",
+
+    const { post } = require("node-fetch");
+    post("https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU", {
         headers: {
             "Content-type": "application/json",
         },
         //Format JSON DATA
         body: JSON.stringify({
             content: `Hi There!\nA new update has been pushed to my server! \nServer-name: \`${Payload.data.app.name}\`\nUpdate: \`Unkown\`\nVersion: \`${Payload.data.app.version}`
-        }),
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
+        })
+    }).then(response => {
         console.log(response);
+    }).catch(err => {
+        console.log(err);
+        res.status(403).send(err);
     })
 };
