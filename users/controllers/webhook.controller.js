4fb26b929fa57c0a122863caf672cd6609ad38d0
Kashima <project-red-alice@outlook.com>
2021-11-19 09:33:35 +1100

    changed architecture to `/webhooks`

---
+++ users/controllers/webhook.controller.js
@@ -0,0 +1,22 @@
+exports.toDiscord = (req, res) => {
+    const Payload = req.body;
+    //Respond To Heroku Webhook
+    res.sendStatus(200);
+    console.log(Object.keys(Payload));
+    const options = {
+        method: "POST",
+        url:
+            "https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU",
+        headers: {
+            "Content-type": "application/json",
+        },
+        //Format JSON DATA
+        body: JSON.stringify({
+            content: `Hi There!\nA new update has been pushed to my server! \nServer-name: \`${Payload.data.app.name}\`\nUpdate: \`Unkown\`\nVersion: \`${Payload.data.app.version}`
+        }),
+    };
+    res.send(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response);
+    })
+};
--- users/routes.config.js
+++ users/routes.config.js
@@ -1,5 +1,5 @@
 const UsersController = require('./controllers/users.controller');
-const request = require('request');
+const WebHook = require("./controllers/webhook.controller");
 
 exports.routesConfig = function (app) {
     app.post('/users', [
@@ -31,26 +31,7 @@ exports.routesConfig = function (app) {
         UsersController.docs
     ]);
     //webhook post path
-    app.post("webhooks", (req, res) => {
-        const Payload = req.body;
-        //Respond To Heroku Webhook
-        res.sendStatus(200);
-        console.log(Object.keys(Payload));
-        const options = {
-            method: "POST",
-            url:
-                "https://discord.com/api/webhooks/909948140436721714/s3E0Z611HJ5w4oY2muWVEIJOGsvwYIZJdHdGvNMo1KMrAoj-yipOLrXd0A8CBN-bHdIU",
-            headers: {
-                "Content-type": "application/json",
-            },
-            //Format JSON DATA
-            body: JSON.stringify({
-                content: `Hi There!\nA new update has been pushed to my server! \nServer-name: \`${Payload.data.app.name}\`\nUpdate: \`Unkown\`\nVersion: \`${Payload.data.app.version}`
-            }),
-        };
-        res.send(options, function (error, response) {
-            if (error) throw new Error(error);
-            console.log(response);
-        })
-    })
+    app.post("webhooks", [
+        WebHook.toDiscord
+    ])
 };
